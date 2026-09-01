6f12ce82074a504891e1f7094ab4282563d8da4d
UtkuKesanli <81084441+UtkuKesanli@users.noreply.github.com>
2026-09-02 02:23:09 +0300

    feat: add social link data

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,28 @@
+export type SocialLink = {
+  label: string;
+  href: string;
+  shortLabel: string;
+};
+
+export const socialLinks: SocialLink[] = [
+  {
+    label: 'GitHub',
+    shortLabel: 'GH',
+    href: 'https://github.com/UtkuKesanli',
+  },
+  {
+    label: 'LinkedIn',
+    shortLabel: 'IN',
+    href: '#',
+  },
+  {
+    label: 'Figma',
+    shortLabel: 'FG',
+    href: '#',
+  },
+  {
+    label: 'Email',
+    shortLabel: '@',
+    href: 'mailto:utkukesanli@gmail.com',
+  },
+];
